16944af846ba675bacb6bca80ab45b162cccf1b2
Ian Dolge <dolge.erau@gmail.com>
2022-08-21 13:05:14 -0700

    Chapter 15: A Better Application Structure

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "SECRET_KEY"
+        "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
